ffe83e4ea7b10b0d3afa89f1f8f1c1fa040e25e0
Raphael Muniz <raphaelmunizvarela@gmail.com>
2026-02-05 23:19:17 -0300

    ok

--- concessionaria/src/app/shared/components/financing/financing.ts
+++ concessionaria/src/app/shared/components/financing/financing.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-financing',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './financing.html',
   styleUrl: './financing.css',
 })
 export class Financing {
-
+  scrollToSection(event: Event, sectionId: string) {
+    event.preventDefault();
+    if (typeof document !== 'undefined') {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }
 }
--- concessionaria/src/main.ts
+++ concessionaria/src/main.ts
@@ -2,5 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+//bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+
+bootstrapApplication(App, appConfig).then(() => {
+  if (typeof window !== 'undefined') {
+    // @ts-ignore
+    lucide.createIcons();
+  }
+});
